4c4ac19afd65e1539a6c281f123de264d41705f6
idane19-meet <idane19@meet.mit.edu>
2018-08-12 17:57:15 +0300

    w

--- databases_final.py
+++ databases_final.py
@@ -1,23 +1,19 @@
-# Database related imports
-# Make sure to import your tables!
+
 from model_final import Base, Article, User 
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# You can change the name of your database, just change project.db to whatever you want (make sure to include .db at the end!)
-# Make sure you have the same name for the database in the app.py file!
+
 engine = create_engine('sqlite:///project.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Your database functions are located under here (querying, adding items, etc.)
 
-# Example of adding a student:
-def add_article(article_title, article_content):
+def add_article(title, content):
     print("Added an article!")
-    article = Article(title=article_title, content=article_content)
+    article = Article(title=title, content=content)
     session.add(article)
     session.commit()
 
@@ -26,9 +22,9 @@ def get_all_articles():
     return articles
 
 
-def add_user(user_nationality, user_name, user_email):
+def add_user(nationality, name, email):
     print("Added a user!")
-    user = User(nationality=user_nationality, name=user_name, email=user_email)
+    user = User(nationality=nationality, name=name, email=email)
     session.add(user)
     session.commit()
 
@@ -39,13 +35,7 @@ def get_all_users():
 
 
 
-def add_student(title, content):
-	
-	article_object = Article(
-		title=title,
-		content=content)
-	session.add(article_object)
-	session.commit()    
+
 
 
 
